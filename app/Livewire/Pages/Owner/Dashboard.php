854b73eed0cf9f89886cbe6441d4dbcc6b2178a9
grannyelf <154070564+grannyelf@users.noreply.github.com>
2026-03-31 13:55:39 +0800

    Added some small layouts as well as prefix for routes, then installed spatie

--- app/Livewire/Pages/Owner/Dashboard.php
+++ app/Livewire/Pages/Owner/Dashboard.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Livewire\Pages\Employee;
+namespace App\Livewire\Pages\Owner;
 
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    #[Layout('components.layouts.employee')]
+    #[Layout('components.layouts.owner')]
     public function render()
     {
-        return view('livewire.pages.employee.dashboard');
+        return view('livewire.pages.owner.dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Livewire\Pages\Admin\Dashboard;
+use App\Livewire\Pages\Employee\Dashboard as Employee;
+use App\Livewire\Pages\Owner\Dashboard as Owner;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('admin.dashboard');
+});
+
+Route::prefix('owner')->group(function () {
+    Route::get('/dashboard', Owner::class)->name('owner.dashboard');
+});
+
+Route::prefix('employee')->group(function () {
+    Route::get('/dashboard', Employee::class)->name('employee.dashboard');
 });
